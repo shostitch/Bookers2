e0fa093a4fd5eb389e3dd083f7aed8943482122f
EC2 Default User <ec2-user@ip-172-31-95-126.ec2.internal>
2022-05-14 06:47:22 +0000

    [Add]課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
@@ -24,6 +24,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about",as: "about"
-  resources :users,only: [:index,:edit]
-  resources :books,only: [:index,:create,:show,:edit,:destroy]
-  resources :books,except: [:new]
+  resources :users,only:[:index,:edit]
+  resources :books,only:[:index,:create,:show,:edit,:update,:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
